c496ca813fe7c5e957697b9f62d278e601825f1e
Jai Sainani <jai.sainani@gmail.com>
2023-01-15 14:41:32 -0800

    added basic scenes

---
+++ Assets/Scripts/SceneManagment.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagment : MonoBehaviour
+{
+    public void LoadNextScreen()
+    {
+        Debug.Log("button load next screen");
+        int nextIndex = SceneManager.GetActiveScene().buildIndex+1;
+        SceneManager.LoadScene(nextIndex);
+    }
+    public void LoadPrevScreen()
+    {
+        int nextIndex = SceneManager.GetActiveScene().buildIndex -1;
+        SceneManager.LoadScene(nextIndex);
+    }
+    public void Exit()
+
+    {
+        Application.Quit();
+    }
+    
+}
