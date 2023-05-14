a393ee86cb278cbbfd649c670eff493d5bbf0bb0
Jai Sainani <jai.sainani@gmail.com>
2023-05-14 14:53:27 -0700

    Added music

---
+++ Assets/Scenes/MusicHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    void Awake() {
+        DontDestroyOnLoad(this);
+    }
+}
